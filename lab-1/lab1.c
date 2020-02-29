77144f79c2bd5b36efa45d655a00b409b6522990
Anh Doan <anh.doan.148@my.csun.edu>
2020-02-29 15:23:13 -0800

    redid code to include function and comments

--- lab-1/lab1.c
+++ lab-1/lab1.c
@@ -6,31 +6,48 @@
 #include <time.h>
 #include <unistd.h>
 
-int main(int argc, char const *argv[]) {
-  time_t startTime = time(NULL);
-  printf("START: %ld\n",startTime);
-  
-  pid_t childProcessId = fork();
-  int status;
-  waitpid(childProcessId, &status, 0);
-  
-  pid_t parentProcessId = getppid();
-  pid_t processId = getpid();
-  printf("PPID: %5d, PID: %5d", parentProcessId, processId);
-  if(childProcessId == 0) {
-    printf("\n");
-    exit(EXIT_SUCCESS);
-  }
-  printf(", CPID: %4d, RETVAL: %d", childProcessId, status); 
-  printf("\n");
-  
-  struct tms timesObj;
-  times(&timesObj);
-  printf("USER:  %4ld, SYS:  %4ld\n", timesObj.tms_utime, timesObj.tms_stime); 
-  printf("CUSER: %4ld, CSYS: %4ld\n", timesObj.tms_cutime, timesObj.tms_cstime); 
+void reportProcInfo(pid_t childProcId) {
+    // Wait until the child process is completed and store its return status
+    int status;
+    waitpid(childProcId, &status, 0);
 
-  time_t stopTime = time(NULL);
-  printf("STOP: %ld\n",stopTime);
+    // Store and print out id of this process and its parent
+    pid_t parentProcId = getppid();
+    pid_t procId = getpid();
+    printf("PPID: %5d, PID: %5d", parentProcId, procId);
   
-  return 0;
+    // This is the child process if childProcId is 0
+    if(childProcId == 0) {
+        printf("\n");
+        return;
+    }
+    // Print out the process id and return value of the child process
+    printf(", CPID: %4d, RETVAL: %d\n", childProcId, status);
+
+    // Store and print out process times
+    struct tms timeObj;
+    times(&timeObj);
+    printf("USER:  %4ld, SYS:  %4ld\n", timeObj.tms_utime, timeObj.tms_stime);
+    printf("CUSER: %4ld, CSYS: %4ld\n", timeObj.tms_cutime, timeObj.tms_cstime);
+}
+
+
+int main(int argc, char const *argv[]) {
+    // Print the start time of the program in unix time (seconds)
+    time_t startTime = time(NULL);
+    printf("START: %ld\n",startTime);
+
+    // Fork the process into a parent and child
+    pid_t childProcId = fork();
+    // Report info about the current process and its child (if applicable)
+    reportProcInfo(childProcId);
+
+    // This is the parent process if childProcId is not 0
+    if (childProcId != 0) {
+        // Print the stop time of the program in unix time (seconds)
+        time_t stopTime = time(NULL);
+        printf("STOP: %ld\n",stopTime);
+    }
+
+    return 0;
 }
